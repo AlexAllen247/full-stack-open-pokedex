361083819e1ba24323ce4a211278019993ec2a19
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-12-29 08:46:59 +0100

    Update for a failed url

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/version', (_req, res) => {
   res.send('2') // change this string to ensure a new version deployed
 })
 
+app.get('/fail', (req, res) => {
+  res.status(404).send()
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
